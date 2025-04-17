894a10ae406a357186929ae13fa67bc4cb103cfd
AlakhPandey1st <physicswallahalakhpandey@gmail.com>
2025-04-18 00:30:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,28 +6,28 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID=15578503
-API_HASH=de14eccfa6fa8d7c2eee9656cc2bdc69
+API_ID = int(15578503) #optional
+API_HASH = "de14eccfa6fa8d7c2eee9656cc2bdc69" #optional
 
 SUDO_USERS =
-OWNER_ID=5525620445
-MONGO_URL=mongodb+srv://Atul:Atul@cluster0.vjd7r1g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-BOT_TOKEN=7762470551:AAFbtj2V7aBnhy-q3vM6qqQ9DIAdeF6mLLI
-ALIVE_PIC=https://telegra.ph/file/3c52a01057865f7511168.jpg
-ALIVE_TEXT=
-PM_LOGGER=
-LOG_GROUP=
-GIT_TOKEN=
+OWNER_ID = int(5525620445)
+MONGO_URL = "mongodb+srv://Atul:Atul@cluster0.vjd7r1g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+BOT_TOKEN = "7762470551:AAFbtj2V7aBnhy-q3vM6qqQ9DIAdeF6mLLI"
+ALIVE_PIC = 'https://telegra.ph/file/3c52a01057865f7511168.jpg'
+ALIVE_TEXT = None
+PM_LOGGER = None
+LOG_GROUP = None
+GIT_TOKEN= None 
 REPO_URL=https://github.com/ITZ-ZAID/ZAID-USERBOT
-BRANCH = getenv("BRANCH", "master") #don't change
+BRANCH = "master" #don't change
  
-STRING_SESSION1=BQDttYcAv_Lgda1aJnZ1tFnfZZ8i_170e3gqpRKvpiNrl_6Cfkttt7sTS-q8v05ec4TozfyNiqEmGMPJLUxaPwhZxuvsQHHT0uYVfRhyp96JppHc33JgYozW_2SI53QVwNtX4cSssdGj1m3n3F1HjgBXTYllpd7MO2sidtTbuHPYeG_i4igg8IUyyUsAZyHWUvmApwSgAfEDW1nwJUJXZC-7-_qdgF8s58YXF2HxElCihJQF2hgCSO_sxqsVqxNq3glkNk5M3VSzB_OA0h4S6hGud52C7elLvUNVOwzvslR5aGJnHvCdVp0o-0IMYYy0KdI_rRi53bjtxvy9fnV411i1yaFTuAAAAAFJWkbdAA
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
-STRING_SESSION6 = getenv("STRING_SESSION6", "")
-STRING_SESSION7 = getenv("STRING_SESSION7", "")
-STRING_SESSION8 = getenv("STRING_SESSION8", "")
-STRING_SESSION9 = getenv("STRING_SESSION9", "")
-STRING_SESSION10 = getenv("STRING_SESSION10", "")
+STRING_SESSION1 = "BQDttYcAv_Lgda1aJnZ1tFnfZZ8i_170e3gqpRKvpiNrl_6Cfkttt7sTS-q8v05ec4TozfyNiqEmGMPJLUxaPwhZxuvsQHHT0uYVfRhyp96JppHc33JgYozW_2SI53QVwNtX4cSssdGj1m3n3F1HjgBXTYllpd7MO2sidtTbuHPYeG_i4igg8IUyyUsAZyHWUvmApwSgAfEDW1nwJUJXZC-7-_qdgF8s58YXF2HxElCihJQF2hgCSO_sxqsVqxNq3glkNk5M3VSzB_OA0h4S6hGud52C7elLvUNVOwzvslR5aGJnHvCdVp0o-0IMYYy0KdI_rRi53bjtxvy9fnV411i1yaFTuAAAAAFJWkbdAA"
+STRING_SESSION2 = ""
+STRING_SESSION3 = ""
+STRING_SESSION4 = ""
+STRING_SESSION5 = ""
+STRING_SESSION6 = ""
+STRING_SESSION7 = ""
+STRING_SESSION8 = ""
+STRING_SESSION9 = ""
+STRING_SESSION10 = ""
